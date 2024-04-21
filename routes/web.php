1855a8b3f57784b1cc26db5ef59162e601995d68
Luciano Scotellaro <scotellaroluciano7@gmail.com>
2024-04-21 17:41:06 +0200

    Add user books route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/users/{user}/books', function(User $user){
+   return view('users.show-books', ['user'=>$user,'books'=>$user->books]);
 });
