5ebe6ec7e32ec592362689d06efd42929e339d75
Luciano Scotellaro <scotellaroluciano7@gmail.com>
2024-04-22 09:14:42 +0200

    Add trades routes, add show book route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\TradeRequestController;
+use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +10,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users/{user}/books', function(User $user){
-   return view('users.show-books', ['user'=>$user,'books'=>$user->books]);
-});
+Route::get('/users/{user}/books', [UserController::class, 'showBooks']);
+
+Route::get('/trades', [TradeRequestController::class, 'index']);
+Route::post('/trades/{book}', [TradeRequestController::class, 'store']);
+Route::get('/trades/{user}/{book}', [TradeRequestController::class, 'show']);
+
+Route::get('/books/{book}', [BookController::class, 'show']);
