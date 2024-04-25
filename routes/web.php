3dbdad3e0634db82e4b0b561f57bcf774848e21b
Salvatore Scotellaro <salvatorescotellaro44@gmail.com>
2024-04-25 13:06:47 +0200

    SCRUM-8 Add generic composer and configuration files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/users/{user}/books', [UserController::class, 'showBooks']);
+
+Route::get('/users/{user}/books/onloan', [UserController::class, 'showBooksOnLoan']);
+Route::get('/users/{user}/books/onloan/create', [UserController::class, 'booksOnLoanCreate']);
+Route::post('/users/{user}/books/{book}/onloan', [UserController::class, 'addBookOnLoan']);
+Route::delete('/users/{user}/books/{book}/onloan', [UserController::class, 'removeBookOnLoan']);
+
+Route::get('/books/search', [BookController::class, 'search']);
