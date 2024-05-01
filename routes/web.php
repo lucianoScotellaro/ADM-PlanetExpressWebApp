16bd12c118bfc29bf63e1734899a8337f7ab5775
Luciano Scotellaro <scotellaroluciano7@gmail.com>
2024-05-01 12:49:13 +0200

    Add accept/refuse trade requests routes

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,21 @@
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\TradeRequestController;
 use App\Http\Controllers\UserController;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+//Users
 Route::get('/users/{user}/books', [UserController::class, 'showBooks']);
 
-Route::get('/trades', [TradeRequestController::class, 'index']);
-Route::post('/trades/{book}', [TradeRequestController::class, 'store']);
-Route::get('/trades/{user}/{book}', [TradeRequestController::class, 'show']);
-
+//Books
 Route::get('/books/{book}', [BookController::class, 'show']);
+
+//Trades
+Route::get('/trades/requests/received/{user}', [TradeRequestController::class, 'index']);
+Route::get('/trades/ask/{user}/{book}', [TradeRequestController::class, 'show']);
+Route::post('/trades/propose/{book}', [TradeRequestController::class, 'store']);
+Route::get('/trades/requests/accept/{sender}/{requestedBook}/{proposedBook}', [TradeRequestController::class, 'update']);
+Route::get('/trades/requests/refuse/{sender}/{requestedBook}/{proposedBook}', [TradeRequestController::class, 'update']);
