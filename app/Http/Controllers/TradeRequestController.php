335dc7556e604dbca1ea6afd8dd531e85c71ba97
Luciano Scotellaro <scotellaroluciano7@gmail.com>
2024-05-01 12:46:28 +0200

    Add trade request update

--- app/Http/Controllers/TradeRequestController.php
+++ app/Http/Controllers/TradeRequestController.php
@@ -11,25 +11,42 @@ class TradeRequestController extends Controller
 {
     public function index(User $user)
     {
-        return view('trades.received.index', ['requests'=>$user->receivedTradeRequests]);
+        return view('trades.received.index', ['requests'=>$user->pendingReceivedTradeRequests]);
     }
 
     public function show(User $user, Book $book){
         session(['receiver'=>$user->id,'requestedBook'=>$book->ISBN]);
-        $activeUser = User::find(2); //current user
+        $activeUser = User::find(2);
         return view('trades.show-propose',['user'=>$activeUser,'books'=>$activeUser->books]);
     }
 
     public function store(Book $book){
-        $user = User::find(2); //current user
-        //Authorization
+        $activeUser = User::find(2);
+
         TradeRequest::create([
             'receiver_id'=>session('receiver'),
-            'sender_id'=>$user->id,
+            'sender_id'=>$activeUser->id,
             'requested_book_ISBN'=>session('requestedBook'),
             'proposed_book_ISBN'=>$book->ISBN
         ]);
 
         return redirect('/');
+    }
+
+    public function update(User $sender, Book $requestedBook, Book $proposedBook){
+        $activeUser = User::find(1);
+        $tradeRequest = TradeRequest::find([$sender->id, $activeUser->id, $proposedBook->ISBN, $requestedBook->ISBN]);
+
+        if(request()->is('trades/requests/accept/*')){
+            $tradeRequest->update([
+                'response'=>true
+            ]);
+        }else{
+            $tradeRequest->update([
+               'response'=>false
+            ]);
+        }
+
+        return redirect('/trades/requests/received/'.$activeUser->id);
     }
 }
