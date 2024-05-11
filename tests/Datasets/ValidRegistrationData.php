f4cddc4fb07546fb6267b0a53390a654c6b38335
Salvatore Scotellaro <salvatorescotellaro44@gmail.com>
2024-05-11 20:03:15 +0200

    SCRUM-3 Add Sign-Up tests and change home route

---
+++ tests/Datasets/ValidRegistrationData.php
@@ -0,0 +1,6 @@
+<?php
+
+dataset('validregistrationdata', [
+            [['name'=>'Valid Name', 'email'=>'arandomemail@gmail.com', 'password'=>'AValidPassword02!', 'password_confirmation'=>'AValidPassword02!'],
+            ['name'=>"D'Angelo", 'email'=>'arandomemail@gmail.com', 'password'=>'AValidPassword02!' ]]
+]);
