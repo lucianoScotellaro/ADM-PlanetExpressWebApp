b0c4b021e3fbd2e228b9057ec26956150e6d29f2
Salvatore Scotellaro <salvatorescotellaro44@gmail.com>
2024-05-12 11:24:05 +0200

    SCRUM-4 Add SessionController

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+
+class SessionController extends Controller
+{
+    public function create(): Factory|\Illuminate\Foundation\Application|View|Application
+    {
+        return view('auth.login');
+    }
+
+    public function store(): RedirectResponse
+    {
+        $validatedData = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if(Auth::attempt($validatedData))
+        {
+            request()->session()->regenerate();
+            return redirect('/');
+        }
+        else
+        {
+            throw ValidationException::withMessages([
+                'credentials-error' => ['Your email or password is incorrect. Please try again.'],
+            ]);
+        }
+    }
+
+    public function destroy(): \Illuminate\Foundation\Application|Redirector|Application|RedirectResponse
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
